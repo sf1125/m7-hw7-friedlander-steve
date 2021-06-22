bb2c685fd445fc2651bdd944d2127a388fef5df9
Steve Friedlander <s.friedlander@ufl.edu>
2021-06-22 13:22:50 -0400

    Correct JS syntax

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
 
-        var header_text = getElementsByClass('header_text');
-        var location= apiResult.name;
+        console.log(apiResult);
+
+        var header_text = document.getElementsByClassName('header_text');
+        var location = apiResult.name;
        
         function welcome_to () {
         header_text.innerHTML = location;
-        }
+        
+        console.log(location);
+    }
 
-        var text = getElementsByClass('text');
+
+        var text = document.getElementsByClassName('text');
         var weather = apiResult.weather.description;
        
         function local_weather () {
         text.innerHTML = weather;
+
+        console.log(weather);
         }
         //Code Dealing With The API Data Goes Here
 
